a0b4a3e2428dc06a29886e9c2a8b1a4ac6d162a8
Rafael Navarro | LA RONDINE <ti01@larondine.com.br>
2026-06-04 15:16:04 -0300

    feat: setup foundation and accessibility components

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,19 @@
+export function Hero() {
+  return (
+    <section>
+      <h1>
+        Saúde inclusiva para todas as pessoas
+      </h1>
+
+      <p>
+        Conectamos você a profissionais preparados
+        para oferecer um atendimento seguro,
+        acolhedor e respeitoso.
+      </p>
+
+      <button>
+        Encontrar profissionais
+      </button>
+    </section>
+  );
+}
